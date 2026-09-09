30c47ab029c886562822f9da86bce1c82729cafb
Yan <yanlucasx10@gmail.com>
2026-09-09 15:58:53 -0300

    Finaliza ex04 e adiciona gitignore

--- ex04.java
+++ ex04.java
@@ -2,3 +2,23 @@
 
 import java.util.Scanner;
 
+public class ex04 {
+    public static int fatorial (int n) {
+        int resultado = 1;
+        for (int i = 1; i <= n; i++) {
+            resultado = resultado * i;
+        }
+        return resultado;
+    }
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        int numero = sc.nextInt();
+
+        int resultado = fatorial(numero);
+        System.out.println("O fatorial de " + numero + " é " + resultado);
+
+        sc.close();
+    }
+}
--- ex05.java
+++ ex05.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class Main {
+public class ex05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
