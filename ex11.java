fa285311da13d8f338c8c59d7e166af785e115c5
João Victor <melo.joaovitor10@gmail.com>
2026-09-12 14:44:54 -0300

    Correção dos exercícios 11 e 12 (nome das classes)

--- ex11.java
+++ ex11.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ex11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] nomes = new String[3];
+        int[] idades = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("--- Pessoa " + (i + 1) + " ---");
+
+            System.out.print("Nome: ");
+            nomes[i] = scanner.nextLine();
+
+            System.out.print("Idade: ");
+            idades[i] = Integer.parseInt(scanner.nextLine());
+        }
+
+        int indiceMaisVelho = 0;
+        for (int i = 1; i < 3; i++) {
+            if (idades[i] > idades[indiceMaisVelho]) {
+                indiceMaisVelho = i;
+            }
+        }
+
+        System.out.println("\nA pessoa mais velha é: " + nomes[indiceMaisVelho]
+                + " (" + idades[indiceMaisVelho] + " anos)");
+
+        scanner.close();
+    }
 }
