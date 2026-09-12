52804e71d8e7ee85ce0a87babc8060ca84ece940
João Victor <melo.joaovitor10@gmail.com>
2026-09-12 14:44:54 -0300

    Resolução do exercício 15

--- ex15.java
+++ ex15.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+//15- Crie um programa que leia um número inteiro e exiba uma contagem regressiva até 0.
+
+import java.util.Scanner;
 
 public class ex15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = sc.nextInt();
+
+        for (int i = numero; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("Fim da contagem!");
+
+        sc.close();
+    }
 }
